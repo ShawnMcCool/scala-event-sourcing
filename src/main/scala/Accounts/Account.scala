1f2bd18f842391609e8a7ebabe30fa33a8feb680
Shawn McCool <shawn@heybigname.com>
2015-02-13 14:00:46 +0100

    updates to language

--- src/main/scala/Accounts/Account.scala
+++ src/main/scala/Accounts/Account.scala
@@ -3,32 +3,38 @@ package Accounts
 import DomainEvents._
 
 object Account {
+
   object ID {
     def generate = ID(java.util.UUID.randomUUID.toString)
   }
+
   case class ID(id: String)
-  
+
   def register(id: ID, name: String): Seq[DomainEvent] = Seq(AccountWasRegistered(id, name))
 
   def fromStream(id: ID, events: Seq[DomainEvent]): Account = {
-    events.foldLeft(Account(id)) { (account, event) => { account.apply(event) }}
+    events.foldLeft(Account(id)) { (account, event) => {
+      account.apply(event)
+    }
+    }
   }
 }
 
 case class Account(id: Account.ID, name: String = "", memberIds: Set[Member.ID] = Set()) {
   def add(m: Member): Seq[DomainEvent] = Seq(new MemberWasAddedToAccount(m.id, this.id))
 
-  def ==(that: Account): Boolean = this.id.equals(that.id)
+  def equals(that: Account): Boolean = this.id.equals(that.id)
 
   // domain event application
-  def apply(e: DomainEvent): Account = e match {
-    case event: AccountWasRegistered => applyAccountWasRegistered(event)
-    case event: MemberWasAddedToAccount => applyMemberWasAddedToAccount(event)
+  def apply(e: DomainEvent): Account = {
+    def applyAccountWasRegistered(e: AccountWasRegistered) =
+      copy(name = e.name)
+    def applyMemberWasAddedToAccount(e: MemberWasAddedToAccount) =
+      copy(memberIds = memberIds + e.memberId)
+
+    e match {
+      case event: AccountWasRegistered    => applyAccountWasRegistered(event)
+      case event: MemberWasAddedToAccount => applyMemberWasAddedToAccount(event)
+    }
   }
-
-  private def applyAccountWasRegistered(e: AccountWasRegistered) =
-    copy(name = e.name)
-
-  private def applyMemberWasAddedToAccount(e: MemberWasAddedToAccount) =
-    copy(memberIds = memberIds + e.memberId)
 }
--- src/test/scala/Accounts/AccountSpec.scala
+++ src/test/scala/Accounts/AccountSpec.scala
@@ -1,19 +1,61 @@
 package Accounts
 
+import DomainEvents._
 import org.scalatest._
 
 class AccountSpec extends FlatSpec with Matchers {
-  "An account" should "accept new members" in {
+  "A generated ID" should "be a unique id" in {
+    Account.ID.generate should not be(Account.ID.generate)
+  }
+
+  "Account registration" should "trigger a AccountWasRegistered event" in {
+    val id = Account.ID.generate
+    val name = "BurgerCo"
+
+    val events = Account.register(id, name)
+
+    events.contains(
+      AccountWasRegistered(id, name)
+    ) should be(true)
+  }
+
+  "A registered account" should "have a name and id" in {
+    val id = Account.ID.generate
+    val name = "BurgerCo"
+    val events: Seq[DomainEvent] = List(AccountWasRegistered(id, name))
+
+    val account = Account.fromStream(id, events)
+    account.id should be(id)
+    account.name should be(name)
+  }
+
+  "Adding a new member" should "throw a MemberWasAddedToAccount event" in {
     // arrange
     val member = Member(Member.ID.generate)
     val accountId = Account.ID.generate
 
     // act
-    val eventStream = Account(accountId).add(member)
+    val events = Account(accountId).add(member)
 
-    // assert
-    val account = Account.fromStream(accountId, eventStream)
+    // assert - events
+    events.contains(
+      MemberWasAddedToAccount(member.id, accountId)
+    ) should be(true)
+  }
+
+  "An account" should "contain an added member" in {
+    val member = Member(Member.ID.generate)
+    val accountId = Account.ID.generate
+
+    val events: Seq[DomainEvent] = List(MemberWasAddedToAccount(member.id, accountId))
+
+    // assert - state
+    val account = Account.fromStream(accountId, events)
     account.memberIds.size should be(1)
     account.memberIds.contains(member.id) should be(true)
+  }
+
+  "An account" should "measure equality by ID" in {
+    Account(Account.ID("1")) should be(Account(Account.ID("1")))
   }
 }
