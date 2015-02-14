88060c15e7b41d046128734984795da6421eb2fe
Shawn McCool <shawn@heybigname.com>
2015-02-14 21:28:01 +0100

    add scalamock library dependency test register member command

--- build.sbt
+++ build.sbt
@@ -4,4 +4,7 @@ version := "0.1"
 
 scalaVersion := "2.10.2"
 
-libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+libraryDependencies ++= Seq(
+  "org.scalatest" % "scalatest_2.10" % "2.0" % "test",
+  "org.scalamock" %% "scalamock-scalatest-support" % "3.2" % "test"
+)
--- src/test/scala/Accounts/Commands/RegisterMemberSpec.scala
+++ src/test/scala/Accounts/Commands/RegisterMemberSpec.scala
@@ -1,7 +1,20 @@
 package Accounts.Commands
 
+import DomainEvents._
+import EventStore._
+import Accounts._
 import org.scalatest._
+import org.scalamock.scalatest.MockFactory
 
-class RegisterMemberSpec extends FlatSpec with Matchers {
+class RegisterMemberSpec extends FlatSpec with Matchers with MockFactory {
+  "The handler" should "register a member" in {
+    val memberId = Member.ID.generate
+    val events: Seq[DomainEvent] = List(MemberHasRegistered(memberId, Email("test@test.com")))
 
+    val eventStore = mock[EventStore]
+    (eventStore.store _).expects(events)
+
+    val handler = new RegisterMemberHandler(eventStore)
+    handler.execute(RegisterMember(memberId, Email("test@test.com")))
+  }
 }
