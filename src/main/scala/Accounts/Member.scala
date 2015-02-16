81ec4b6d1c325e91ec7bb1fa7f4b403f86f68f93
Shawn McCool <shawn@heybigname.com>
2015-02-16 14:05:56 +0100

    clean up

--- src/main/scala/Accounts/Member.scala
+++ src/main/scala/Accounts/Member.scala
@@ -6,22 +6,16 @@ import EventStore._
 object Member extends Aggregate[Member] {
   def register(id: Member.ID, email: Email): Seq[DomainEvent] = Seq(MemberHasRegistered(id, email))
 
-  // Apply Domain Events
   def applyEvents(e: DomainEvent, member: Option[Member]): Member = e match {
     case event: MemberHasRegistered     => Member(event.id, event.email)
     case event: MemberChangedTheirEmail => member.get.copy(id = event.id, email = event.email)
     case _                              => throw new UnmatchedDomainEvent(e)
   }
 
-  // Member ID
   object ID {
-    def empty = ID("")
     def generate = ID(java.util.UUID.randomUUID.toString)
   }
-  case class ID(id: String) extends AggregateIdentity {
-    def equals(that: ID): Boolean = this.id == that.id
-  }
-
+  case class ID(id: String) extends AggregateIdentity
 }
 
 case class Member(id: Member.ID, email: Email) {
